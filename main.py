e540267bf98bf116f625ef0d1479aeb979cd3dad
LENOVO <fabricefadegnon@gmail.com>
2024-06-06 17:29:06 +0200

    added the last comment

--- main.py
+++ main.py
@@ -19,3 +19,5 @@ english_text = translate_french_to_english(french_text)
 # Print the original French text and the translated English text
 print(f"Original French: {french_text}")
 print(f"Translated English: {english_text}")
+
+#Thanks to Stan I learnt this
